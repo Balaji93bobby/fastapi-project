5edf7aa6afac7c91d10e9ec931f8e190f05e5e1b
Balaji <balaji@abshrms.com>
2025-07-16 20:29:33 +0530

    implemendted schema models using pydantic

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from struct import pack
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 @app.get('/')
 def hello() -> dict:
@@ -12,6 +23,7 @@ def post() -> dict:
     return {'data': 'this is the post'}
 
 @app.post('/createposts')
-def create_posts(payload: dict = Body(...)) -> dict:
-    return {'message': 'post created successfully', "post": payload}
+def create_posts(new_post: Post) -> dict:
+    print(new_post)
+    return {'message': 'post created successfully', "post": new_post}
 
