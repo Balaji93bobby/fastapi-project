4c0ec337254a0d452c74ce6b8f2408f64467d45b
balaji93bobby <balaji93bobby@gmail.com>
2025-07-24 08:09:17 +0530

    used sqlalchemy to delete a post in the database based on id

--- app/main.py
+++ app/main.py
@@ -106,14 +106,15 @@ def get_post(id: int, db: Session = Depends(get_db)):
         }
 
 @app.delete('/post/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute(""" DELETE FROM posts WHERE id = (%s) RETURNING *""", str((id)))
-    post = cursor.fetchone()
-    conn.commit()
-    if post == None:
+def delete_post(id: int, db: Session=Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'the post with id: {id} is not found'
                         )
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
