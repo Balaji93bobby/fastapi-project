8cbbb64e14102b2166b422d070bf44d5122b256d
balaji93bobby <balaji93bobby@gmail.com>
2025-07-24 09:47:39 +0530

    removed unwanted functions and  cleaned the code

--- app/main.py
+++ app/main.py
@@ -18,49 +18,7 @@ class PostSchema(BaseModel):
     content: str
     published: bool = True
 
-while True:
-    try:
-        conn = psycopg2.connect(
-            host='localhost', 
-            database='fastapi', 
-            user='balaji', 
-            password='balaji',
-            cursor_factory=RealDictCursor
-        )
-        cursor=conn.cursor()
-        print('database connection successfull')
-        break
-    except Exception as error:
-        print(f'database conenction error: {error}')
-        time.sleep(2)
 
-my_posts = [
-        {
-            'title': 'top beaches in chenn',
-            'id': 1, 
-            'content': 'Checkout these awesome beaches', 
-            'published': True, 
-            'rating': 5
-        }, 
-        {
-            'title': 'top beaches in chennai', 
-            'id': 2, 
-            'content': 'Checkout these awesome beaches', 
-            'published': True, 
-            'rating': 5
-        }
-    ]
-
-
-def find_post(id):
-    for post in my_posts:
-        if post['id'] == id:
-            return post
-        
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
 
 @app.get('/')
 def hello() -> dict:
@@ -68,19 +26,14 @@ def hello() -> dict:
         "message" : "Hello World!"
     }
 
+
+
 @app.get('/posts')
-# def get_posts() -> dict:
-#     cursor.execute("""SELECT * FROM posts """)
-#     posts = cursor.fetchall()
-#     return {"data": posts}
 async def test(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return {'data': posts}
 
-# @app.get('/sql')
-# async def test(db: Session = Depends(get_db)):
-#     posts = db.query(models.Post).all()
-#     return {'data': posts}
+
 
 @app.post('/posts', status_code=status.HTTP_201_CREATED)
 def create_post(new_post: PostSchema, db: Session = Depends(get_db)) :
@@ -115,26 +68,6 @@ def delete_post(id: int, db: Session=Depends(get_db)):
     post.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-# @app.put('/post/{id}', status_code=status.HTTP_202_ACCEPTED)
-# def update_post(id: int, post: PostSchema, db: Session=Depends(get_db)):
-#         query_post = db.query(models.Post).filter(models.Post.id == id)
-#         # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
-#         # updated_post = cursor.fetchone()
-#         # conn.commit()
-#         if query_post == None:
-#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-#                             detail=f'the post with id: {id} is not found'
-#                         )
-#         updated_post = query_post.update(post.model_dump(), synchronize_session=False)
-#         db.commit()
-#         db.refresh(updated_post)
-#         return{
-#             'update_status': f"The post with id: {id} is updated",
-#             'data': updated_post
-#         }
-
 
 @app.put('/post/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update_post(id: int, post: PostSchema, db: Session = Depends(get_db)):
