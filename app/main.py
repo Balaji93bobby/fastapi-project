c5b82879de817714c7836cffb737b9e04e37b91e
balaji93bobby <balaji93bobby@gmail.com>
2025-07-24 20:32:13 +0530

    created rotue to get user based on id

--- app/main.py
+++ app/main.py
@@ -96,3 +96,12 @@ def create_user(new_user: CreateUser, db: Session = Depends(get_db)):
 def get_users(db: Session = Depends(get_db)):
     user_list = db.query(models.User).all()
     return user_list
+
+@app.get('/users/{id}', response_model=UserDetail)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="the user with id: {id} does not exist"
+                            )
+    return user
