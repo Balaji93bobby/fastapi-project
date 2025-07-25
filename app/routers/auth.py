e532e43bfd27abe43d85413a96cd6072cf9a0b3d
balaji93bobby <balaji93bobby@gmail.com>
2025-07-25 12:47:00 +0530

    Used native oauthpasswordrequestform instead of suing json for login function

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,15 +1,14 @@
-import stat
 from fastapi import APIRouter , Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from app.database import get_db
-from .. import schemas, database, models, utils, oauth2
-
+from .. import schemas, models, utils, oauth2
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-async def login(user_creds: schemas.UserLogin,db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_creds.email).first()
+async def login(user_creds: OAuth2PasswordRequestForm = Depends(),db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_creds.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail='Invalid Credentials')
