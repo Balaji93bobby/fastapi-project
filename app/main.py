90a4c9abf04e7eb0dd547290eecae59ef18713d6
balaji93bobby <balaji93bobby@gmail.com>
2025-08-12 14:42:29 +0530

    added cors middelware

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import Body, FastAPI
 # from . import models
 from .routers import post, user, vote, auth
 # from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # models.Base.metadata.create_all(bind=engine) this si used to create ak the tables at the start of the applicaiton
@@ -11,7 +12,18 @@ from .routers import post, user, vote, auth
 
 
 app = FastAPI()
+origins=['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=['*'],
+    allow_credentials=True,
+    allow_headers=['*']
+)
 
+@app.get('/')
+def Hello():
+    return {'hello': 'world'}
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
